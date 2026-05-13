f8a4df1faeff849b54143e1ba03c9433e32af1e4
ghxm <37017654+ghxm@users.noreply.github.com>
2026-05-13 18:14:13 +0200

    wrap merge in keep_attributes to preserve dataset S3 class
    merge() strips custom S3 classes, causing composition() to error with
    "Please supply a dataset object". The original [which()] subsetting
    preserved the class, but the merge replacement did not. Wrapping in
    keep_attributes() is consistent with the date filter step above.

--- R/eu_composition.R
+++ R/eu_composition.R
@@ -95,8 +95,9 @@ ep <- function(date, data, linktable, comittee_name, type='plenary'){
         country_election_max <- aggregate(parlgov_election_ep[,'election_date'], list(parlgov_election_ep[,'country_id']), max)
         colnames(country_election_max) <- c('country_id', 'election_date')
 
-        parlgov_election_ep <- merge(parlgov_election_ep, country_election_max,
-                                     by = c('country_id', 'election_date'))
+        parlgov_election_ep <- keep_attributes(merge(parlgov_election_ep, country_election_max,
+                                                      by = c('country_id', 'election_date')),
+                                                parlgov_election_ep)
 
         # return composititon object
         return(composition(parlgov_election_ep, from = 'parlgov', 'EP plenary', type='parlgov_election', date=date, linktable=linktable))
